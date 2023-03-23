7ea57a408e1e8afc9cd72fd2dd07682d52f5822e
shamail chaudry <shamail648@gmail.com>
2023-03-23 23:34:23 +0000

    fixing rides.csv file not found error

--- mdc_invoice_generator/settings.py
+++ mdc_invoice_generator/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'invoice_generator/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "invoice_generator/static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'rides.csv'), # add this line to include the rides.csv file
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
