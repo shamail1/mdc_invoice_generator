ef2328401c33e8613e78964a1932a46a484ee996
shamail chaudry <shamail648@gmail.com>
2023-03-25 00:54:48 +0000

    add functionality to delete data

--- invoice_generator/urls.py
+++ invoice_generator/urls.py
@@ -3,6 +3,7 @@ from .views import upload_csv
 from .views import log_ride
 from .views import view_bookings
 from .views import home
+from .views import delete_row
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
@@ -10,6 +11,7 @@ urlpatterns = [
     path('create_invoice/', upload_csv, name='upload_csv'),
     path('input_booking_data/', log_ride, name='log_ride'),
     path('view_booking_data/', view_bookings, name='view_bookings'),
+    path('delete/<str:id>/', delete_row, name='delete_row'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
